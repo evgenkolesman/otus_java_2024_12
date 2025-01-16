c7145708581c7173b2a09fef3f0a250c1e12156d
Evgeniy Kolesnikov <evgeniy.kolesnikov@devexperts.com>
2025-01-16 22:42:26 +0300

    13. Test and classes fixed

--- hw03-generics/src/main/java/homework/Customer.java
+++ hw03-generics/src/main/java/homework/Customer.java
@@ -1,9 +1,6 @@
 package homework;
 
-import java.util.Objects;
-
-@SuppressWarnings({"java:S1135"}) // при выполнении ДЗ эту аннотацию надо удалить
-public class Customer {
+public class Customer implements Comparable<Customer> {
     private final long id;
     private String name;
     private long scores;
@@ -48,16 +45,24 @@ public class Customer {
 
         Customer customer = (Customer) o;
 
-        if (id != customer.id) return false;
-        if (scores != customer.scores) return false;
-        return Objects.equals(name, customer.name);
+        return id == customer.id;
     }
 
     @Override
     public int hashCode() {
-        int result = Long.hashCode(id);
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + Long.hashCode(scores);
-        return result;
+        return Long.hashCode(id);
+    }
+
+    @Override
+    public int compareTo(Customer o) {
+        if (o == null) return -1;
+        if (this.id > o.getId()) return 1;
+        if (this.id < o.getId()) {
+            return -1;
+        }
+        if (this.id == o.getId()) {
+            return 0;
+        }
+        return -1;
     }
 }
--- hw03-generics/src/main/java/homework/CustomerService.java
+++ hw03-generics/src/main/java/homework/CustomerService.java
@@ -1,21 +1,35 @@
 package homework;
 
+import java.util.AbstractMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
-@SuppressWarnings({"java:S1186", "java:S1135", "java:S1172"}) // при выполнении ДЗ эту аннотацию надо удалить
 public class CustomerService {
 
     // todo: 3. надо реализовать методы этого класса
+    private final TreeMap<Customer, String> treeMap = new TreeMap<>(Comparator.comparing(Customer::getScores));
     // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
     public Map.Entry<Customer, String> getSmallest() {
         // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> customerStringEntry = treeMap.firstEntry();
+        Customer customer = customerStringEntry.getKey();
+        return new AbstractMap.SimpleEntry<>(
+                new Customer(customer.getId(), customer.getName(), customer.getScores()),
+                customerStringEntry.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+
+        return treeMap.entrySet().stream()
+                .filter(c -> c.getKey().getScores() >= customer.getScores())
+                .min(Comparator.comparingLong(entry -> entry.getKey().getScores()))
+                .<Map.Entry<Customer, String>>map(AbstractMap.SimpleEntry::new)
+                .orElse(null);
     }
 
-    public void add(Customer customer, String data) {}
+    public void add(Customer customer, String data) {
+        treeMap.put(new Customer(customer.getId(), customer.getName(), customer.getScores()), data);
+    }
 }
