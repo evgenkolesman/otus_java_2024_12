469be57c49644aec8f541baa1ad01b6529a3a9fc
Evgeniy Kolesnikov <evgeniy.kolesnikov@devexperts.com>
2025-02-23 20:12:45 +0300

    22. aop hw added reflection to constructor handler

--- hw10-aspect/src/main/java/ru/otus/classrunner/ClassRunner.java
+++ hw10-aspect/src/main/java/ru/otus/classrunner/ClassRunner.java
@@ -1,16 +1,8 @@
 package ru.otus.classrunner;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import ru.otus.annotation.Log;
 
 public class ClassRunner {
-
-    static final List<Method> list = new ArrayList<>();
 
     /**
      * Instance with no args constructor
@@ -19,19 +11,8 @@ public class ClassRunner {
      * @return proxy
      */
     public static Object proxifier(Object instance) {
-        try {
-            Arrays.stream(instance.getClass().getDeclaredMethods()).forEach(m -> {
-                if (m.isAnnotationPresent(Log.class)) {
-                    list.add(m);
-                }
-            });
 
-            final InvocationHandler handler = new DemoInvocationHandler(instance, new ArrayList<>(list));
-            final Class<?>[] interfaces = instance.getClass().getInterfaces();
-
-            return Proxy.newProxyInstance(ClassRunner.class.getClassLoader(), interfaces, handler);
-        } finally {
-            list.clear();
-        }
+        final DemoInvocationHandler handler = new DemoInvocationHandler(instance);
+        return Proxy.newProxyInstance(ClassRunner.class.getClassLoader(), handler.interfaces(), handler);
     }
 }
--- hw10-aspect/src/main/java/ru/otus/classrunner/DemoInvocationHandler.java
+++ hw10-aspect/src/main/java/ru/otus/classrunner/DemoInvocationHandler.java
@@ -1,35 +1,52 @@
 package ru.otus.classrunner;
 
-import java.lang.reflect.*;
-import java.util.*;
-import java.util.function.*;
-import org.slf4j.*;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.annotation.Log;
 
 class DemoInvocationHandler implements InvocationHandler {
     private static final Logger logger = LoggerFactory.getLogger(DemoInvocationHandler.class);
 
     private final Object myClass;
-    private final List<Method> methodList;
+    private final Class<?>[] interfaces;
+    private final Set<Method> methodSet;
 
-    DemoInvocationHandler(Object myClass, List<Method> methodList) {
-        this.myClass = myClass;
-        this.methodList = methodList;
+    DemoInvocationHandler(Object instance) {
+        this.myClass = instance;
+        this.methodSet = new HashSet<>();
+        interfaces = myClass.getClass().getInterfaces();
+
+        Arrays.stream(instance.getClass().getDeclaredMethods()).forEach(m -> {
+            if (m.isAnnotationPresent(Log.class)) {
+                Arrays.stream(interfaces)
+                        .flatMap(interfacesClass -> Arrays.stream(interfacesClass.getDeclaredMethods()))
+                        .filter(getMethodPredicate(m))
+                        .findFirst()
+                        .ifPresent(methodSet::add);
+            }
+        });
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (methodList.stream()
-                .filter(m -> m.getName().equals(method.getName()))
-                .anyMatch(getMethodPredicate(method, args))) {
+        if (methodSet.contains(method)) {
             logger.info("invoking method:{}, arguments: {}", method, args);
         }
-
         return method.invoke(myClass, args);
     }
 
-    private Predicate<Method> getMethodPredicate(Method method, Object[] args) {
-        return m -> m.getParameterCount() == args.length
-                && Arrays.equals(m.getParameterTypes(), method.getParameterTypes())
+    public Class<?>[] interfaces() {
+        return interfaces;
+    }
+
+    private Predicate<Method> getMethodPredicate(Method method) {
+        return m -> Arrays.equals(m.getParameterTypes(), method.getParameterTypes())
                 && m.getReturnType().equals(method.getReturnType());
     }
 
